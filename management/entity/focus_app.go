8e51ee116e8897ac117507a11273bb4db461d4fe
Juanma Barea Martinez <juanmabareamartinez@gmail.com>
2024-01-03 17:43:34 +0100

    Adds maven project fields into FocusApp

--- management/entity/focus_app.go
+++ management/entity/focus_app.go
@@ -6,14 +6,20 @@ type FocusApp struct {
 	SourceRepository string `json:"source_repository" bson:"source_repository,omitempty"`
 	ConfigRepository string `json:"config_repository" bson:"config_repository,omitempty"`
 	ImageRegistry    string `json:"image_registry" bson:"image_registry,omitempty"`
+	ArtifactId       string `json:"artifact_id" bson:"artifact_id"`
+	GroupId          string `json:"group_id" bson:"group_id"`
+	Package          string `json:"package" bson:"package"`
 }
 
-func NewFocusApp(n string, t string, sr string, cr string, ir string) *FocusApp {
+func NewFocusApp(n string, t string, sr string, cr string, ir string, ai string, gi string, pkg string) *FocusApp {
 	return &FocusApp{
 		Name:             n,
 		Type:             t,
 		SourceRepository: sr,
 		ConfigRepository: cr,
 		ImageRegistry:    ir,
+		ArtifactId:       ai,
+		GroupId:          gi,
+		Package:          pkg,
 	}
 }
