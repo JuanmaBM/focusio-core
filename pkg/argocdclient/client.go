09c93274e87ffe1c8670043bdc29ac611af5e326
Juanma Barea <juanmabareamartinez@gmail.com>
2024-11-23 19:06:50 +0100

    Add automatic Oauth generation token and modify the client public functions to use this new aproach
    Signed-off-by: Juanma Barea <juanmabareamartinez@gmail.com>

--- pkg/argocdclient/client.go
+++ pkg/argocdclient/client.go
@@ -1,23 +1,73 @@
 package argocdclient
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"sync"
 
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 )
 
-const OPEN_CONNECTION_FAILED_MESSAGE string = "Failed to open a connection to ArgoCD server: %v"
-
 type ArgoCDClient struct {
-	client apiclient.Client
-	ctx    context.Context
+	client     apiclient.Client
+	ctx        context.Context
+	tokenLock  sync.Mutex
+	username   string
+	password   string
+	serverAddr string
+	authToken  string
+	insecure   bool
 }
 
-func NewArgoCDClient(serverAddr, authToken string, insecure bool, caCertPath string) (*ArgoCDClient, error) {
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func getAuthToken(serverAddr string, username string, password string) (string, error) {
+	loginURL := serverAddr + "/api/v1/session"
+	loginRequest := LoginRequest{
+		Username: username,
+		Password: password,
+	}
+
+	reqBody, err := json.Marshal(loginRequest)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(loginURL, "application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return "", fmt.Errorf("failed to login: %s", string(bodyBytes))
+	}
+
+	var loginResponse LoginResponse
+	if err := json.NewDecoder(resp.Body).Decode(&loginResponse); err != nil {
+		return "", err
+	}
+
+	return loginResponse.Token, nil
+}
+
+func createArgcdClient(serverAddr string, authToken string, insecure bool) (apiclient.Client, error) {
 
 	clientOpts := &apiclient.ClientOptions{
 		ServerAddr: serverAddr,
@@ -29,76 +79,126 @@ func NewArgoCDClient(serverAddr, authToken string, insecure bool, caCertPath str
 
 	client, err := apiclient.NewClient(clientOpts)
 	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func NewArgoCDClient(serverAddr string, username string, password string, insecure bool, caCertPath string) (*ArgoCDClient, error) {
+
+	authToken, err := getAuthToken(serverAddr, username, password)
+	if err != nil {
+		log.Fatalf("Client can't get Authorization Token from ArgoCD with the crendetials provided")
+		return nil, err
+	}
+
+	client, err := createArgcdClient(serverAddr, authToken, insecure)
+	if err != nil {
 		log.Fatalf("Failed to create ArgoCD client: %v", err)
 		return nil, err
 	}
 
 	return &ArgoCDClient{
-		client: client,
-		ctx:    context.Background(),
+		client:     client,
+		ctx:        context.Background(),
+		username:   username,
+		password:   password,
+		serverAddr: serverAddr,
+		authToken:  authToken,
+		insecure:   insecure,
 	}, nil
+}
+
+func (c *ArgoCDClient) DoRequestWithRetry(requestFunc func(appClient application.ApplicationServiceClient) error) error {
+
+	conn, appClient, err := c.client.NewApplicationClient()
+	if err != nil {
+		return fmt.Errorf("failed to open a connection to ArgoCD server: %v", err)
+	}
+	defer conn.Close()
+
+	err = requestFunc(appClient)
+
+	if err != nil && isUnauthorized(err) {
+
+		c.tokenLock.Lock()
+		defer c.tokenLock.Unlock()
+
+		authToken, err := getAuthToken(c.serverAddr, c.username, c.password)
+		if err != nil {
+			return fmt.Errorf("error renewing auth token: %v", err)
+		}
+
+		c.authToken = authToken
+		c.client, err = createArgcdClient(c.serverAddr, authToken, c.insecure)
+		if err != nil {
+			return fmt.Errorf("error recreating ArgoCD client with new token: %v", err)
+		}
+
+		err = requestFunc(appClient)
+	}
+
+	return err
 }
 
 func (c *ArgoCDClient) ListApplications() (*v1alpha1.ApplicationList, error) {
 
-	conn, appClient, err := c.client.NewApplicationClient()
-	if err != nil {
-		log.Fatalf("Failed to open a connection to ArgoCD server: %v", err)
-		return nil, err
-	}
-	defer conn.Close()
+	var apps *v1alpha1.ApplicationList
+	err := c.DoRequestWithRetry(func(appClient application.ApplicationServiceClient) error {
+		appList, err := appClient.List(c.ctx, &application.ApplicationQuery{})
+		if err != nil {
+			return fmt.Errorf("failed to get all applications: %v", err)
+		}
+		apps = appList
+		return err
+	})
 
-	apps, err := appClient.List(c.ctx, &application.ApplicationQuery{})
-	if err != nil {
-		log.Printf("Failed to get all applications: %v", err)
-		return nil, err
-	}
+	return apps, err
 
-	return apps, nil
 }
 
-func (c *ArgoCDClient) CreateApplication(application *application.ApplicationCreateRequest) (*v1alpha1.Application, error) {
+func (c *ArgoCDClient) CreateApplication(newApp *application.ApplicationCreateRequest) (*v1alpha1.Application, error) {
 
-	if application == nil {
+	if newApp == nil {
 		return nil, errors.New("application must be defined")
 	}
 
-	conn, appClient, err := c.client.NewApplicationClient()
-	if err != nil {
-		log.Fatalf("Failed to open a connection to ArgoCD server: %v", err)
-		return nil, err
-	}
-	defer conn.Close()
+	var applicationCreated *v1alpha1.Application
+	err := c.DoRequestWithRetry(func(appClient application.ApplicationServiceClient) error {
+		app, err := appClient.Create(c.ctx, newApp)
+		if err != nil {
+			return fmt.Errorf("application can not be created: %v", err)
+		}
+		applicationCreated = app
+		return err
+	})
 
-	applicationCreated, err := appClient.Create(c.ctx, application)
-	if err != nil {
-		log.Fatalf("Application can not be created: %v", err)
-		return nil, err
-	}
-	return applicationCreated, nil
+	return applicationCreated, err
 }
 
 func (c *ArgoCDClient) GetApplication(query application.ApplicationQuery) (*v1alpha1.Application, error) {
 
 	if isEmpty(query) {
-		return nil, errors.New("Application name parameter must be defined")
+		return nil, errors.New("application name parameter must be defined")
 	}
 
-	conn, appClient, err := c.client.NewApplicationClient()
-	if err != nil {
-		log.Fatalf(OPEN_CONNECTION_FAILED_MESSAGE, err)
-		return nil, err
-	}
-	defer conn.Close()
+	var foundApp *v1alpha1.Application
+	err := c.DoRequestWithRetry(func(appClient application.ApplicationServiceClient) error {
+		app, err := appClient.Get(c.ctx, &query)
+		if err != nil {
+			log.Fatalf("Application not found with query: %v", query)
+		}
+		foundApp = app
+		return nil
+	})
 
-	application, err := appClient.Get(c.ctx, &query)
-	if err != nil {
-		log.Fatalf("Application not found with query: %v", query)
-		return nil, err
-	}
-	return application, nil
+	return foundApp, err
 }
 
 func isEmpty(query application.ApplicationQuery) bool {
 	return query.Name == nil
+}
+
+func isUnauthorized(err error) bool {
+	return true
 }
