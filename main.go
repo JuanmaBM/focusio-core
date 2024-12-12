7c08dd5081ec9d2a45ba7870dc5f8fbed295978f
Juanma Barea <juanmabareamartinez@gmail.com>
2024-12-12 17:56:16 +0100

    Adds creation ArgoCD client into main
    Signed-off-by: Juanma Barea <juanmabareamartinez@gmail.com>

--- main.go
+++ main.go
@@ -7,17 +7,13 @@ import (
 	"github.com/juanmabm/focusio-core/internal/database"
 	"github.com/juanmabm/focusio-core/internal/focusapp"
 	"github.com/juanmabm/focusio-core/internal/focuscatalog"
+	"github.com/juanmabm/focusio-core/pkg/argocdclient"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func SetupHttpServer(dbConnection *mongo.Database) *gin.Engine {
+func setupHttpServer() *gin.Engine {
 
 	r := gin.Default()
-
-	far := focusapp.NewFocusAppRepository(dbConnection)
-	fcr := focuscatalog.NewFocusCatalogItemRepository(dbConnection)
-	focusapp.RegisterHandlers(r, far, fcr)
-	focuscatalog.RegisterHandlers(r, fcr)
 
 	r.GET("/health", func(c *gin.Context) {
 		c.Status(200)
@@ -26,7 +22,7 @@ func SetupHttpServer(dbConnection *mongo.Database) *gin.Engine {
 	return r
 }
 
-func CreateDatabaseConnection() *mongo.Database {
+func createDatabaseConnection() *mongo.Database {
 
 	var hostname = os.Getenv("MONGO_HOSTNAME")
 	var username = os.Getenv("MONGO_USERNAME")
@@ -42,8 +38,31 @@ func CreateDatabaseConnection() *mongo.Database {
 	return dbConnection
 }
 
+func createArgocdConnection() *argocdclient.ArgoCDClient {
+
+	var serverAddr = os.Getenv("ARGOCD_SERVER_ADDR")
+	var port = os.Getenv("ARGOCD_SERVER_PORT")
+	var username = os.Getenv("ARGOCD_USERNAME")
+	var password = os.Getenv("ARGOCD_PASSWORD")
+	var insecure bool = os.Getenv("ARGOCD_CONNECTION_INSECURE") == "true"
+
+	ac, err := argocdclient.NewArgoCDClient(serverAddr, port, username, password, insecure)
+	if err != nil {
+		panic(err)
+	}
+	return ac
+}
+
 func main() {
-	dbConnection := CreateDatabaseConnection()
-	httpServer := SetupHttpServer(dbConnection)
-	httpServer.Run(":8080")
+	dbConnection := createDatabaseConnection()
+	// TODO: Add Argocd client to handlers
+	// ac := createArgocdConnection()
+	r := setupHttpServer()
+
+	far := focusapp.NewFocusAppRepository(dbConnection)
+	fcr := focuscatalog.NewFocusCatalogItemRepository(dbConnection)
+	focusapp.RegisterHandlers(r, far, fcr)
+	focuscatalog.RegisterHandlers(r, fcr)
+
+	r.Run(":8080")
 }
